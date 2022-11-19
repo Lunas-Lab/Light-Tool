95769642bfd9401fa97d7584efda3295a3dcda75
HappyTogetherTurt <72428016+HappyTogetherTurt@users.noreply.github.com>
2022-11-19 16:48:29 +1300

    Fully functional and laid out

--- script.js
+++ script.js
@@ -8,12 +8,10 @@ function panelTapped() {
 }
 
 function sliderMove() {
-    var panel = $("panel");
     $('redLabel').innerText = "Red: " + $('redSlider').value;
     $('greenLabel').innerText = "Green: " + $('greenSlider').value;
     $('blueLabel').innerText = "Blue: " + $('blueSlider').value;
-    panel.style.backgroundColor = "rgb(" + $('redSlider').value + ", " + $('greenSlider').value + ", " + $('blueSlider').value + ")";
-    console.log("rgb(" + $('redSlider').value + ", " + $('greenSlider').value + ", " + $('blueSlider').value + ")");
+    $('panel').style.backgroundColor = "rgb(" + $('redSlider').value + ", " + $('greenSlider').value + ", " + $('blueSlider').value + ")";
 }
 
 function closeControls() {
